bf7653936e41a5361e5bfc0622608379bcafb35b
SzokeB <bence.szoke@idomsoft.hu>
2022-04-29 01:55:08 +0200

    add windowedBy

--- src/main/java/szoeke/bence/kafkastreamprocessor/processor/StreamProcessor.java
+++ src/main/java/szoeke/bence/kafkastreamprocessor/processor/StreamProcessor.java
@@ -4,15 +4,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.*;
 import szoeke.bence.kafkastreamprocessor.entity.Event;
 import szoeke.bence.kafkastreamprocessor.entity.innerentity.SipMessage;
 import szoeke.bence.kafkastreamprocessor.utility.EventDeserializer;
 import szoeke.bence.kafkastreamprocessor.utility.EventSerializer;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
@@ -43,20 +44,41 @@ public class StreamProcessor {
     }
 
     private void defineOperations() {
-        builder
-                .stream(INPUT_TOPIC, Consumed.with(longSerde, eventSerde))
-                .mapValues(this::mapEvent)
-                .to(OUTPUT_TOPIC, Produced.with(longSerde, eventSerde));
+        try (TimeWindowedSerializer<Long> serializer = new TimeWindowedSerializer<>(Serdes.Long().serializer());
+             TimeWindowedDeserializer<Long> deserializer = new TimeWindowedDeserializer<>(Serdes.Long().deserializer())) {
+            builder
+                    .stream(INPUT_TOPIC, Consumed.with(longSerde, eventSerde))
+                    .peek((key, event) -> System.out.printf("Actual key is %d.%n", key))
+//                    .filter(this::filterEventRecordHeaderResult)
+//                    .map(this::mapEventRecordHeaderKeyIds)
+//                    .map(this::mapHeaderFieldName)
+                    .groupByKey()
+                    .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofMillis(2500)))
+                    .count()
+                    .toStream()
+                    .peek((key, value) -> System.out.printf("Output key and value is: %d | %d%n", key.key(), value))
+                    .to(OUTPUT_TOPIC, Produced.with(Serdes.serdeFrom(serializer, deserializer), Serdes.Long()));
+        }
+    }
+
+    //filter by result
+    private boolean filterEventRecordHeaderResult(Long key, Event event) {
+        return event.eventRecordHeader.Result == 1;
+    }
+
+    //remove KeyIds
+    private KeyValue<Long, Event> mapEventRecordHeaderKeyIds(Long key, Event event) {
+        event.eventRecordHeader.KeyIds = null;
+        return KeyValue.pair(key, event);
     }
 
     //remove headerfield list items where headerfield name equals to "From", "To" or "Via"
-
-    private Event mapEvent(Long key, Event event) {
+    private KeyValue<Long, Event> mapHeaderFieldName(Long key, Event event) {
         event.eventInfo.SipMessages = event.eventInfo.SipMessages
                 .parallelStream()
                 .map(this::mapSipMessage)
                 .collect(Collectors.toList());
-        return event;
+        return KeyValue.pair(key, event);
     }
 
     private SipMessage mapSipMessage(SipMessage sipMessage) {
@@ -85,19 +107,4 @@ public class StreamProcessor {
         }
         System.exit(0);
     }
-
-//remove KeyIds
-//    private Event mapValues(Long object, Event event) {
-//        event.eventRecordHeader.KeyIds = null;
-//        return event;
-//    }
-
-
-//filter by result:
-//    private void defineOperations() {
-//        builder
-//                .stream(INPUT_TOPIC, Consumed.with(longSerde, eventSerde))
-//                .filter(event -> event.eventRecordHeader.Result == 1)
-//                .to(OUTPUT_TOPIC, Produced.with(longSerde, eventSerde));
-//    }
 }

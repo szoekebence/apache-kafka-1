326138b2f40077df699d137feae72c921c62ff6a
SzokeB <bence.szoke@idomsoft.hu>
2022-05-01 23:18:34 +0200

    refact

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM openjdk:16-alpine3.13
+FROM openjdk:18
 WORKDIR /app
 ADD /target/kafka-stream-processor-fatjar.jar /app/kafka-stream-processor.jar
+EXPOSE 8080
 CMD ["java", "-jar", "/app/kafka-stream-processor.jar"]
--- src/main/java/szoeke/bence/kafkastreamprocessor/processor/StreamProcessor.java
+++ src/main/java/szoeke/bence/kafkastreamprocessor/processor/StreamProcessor.java
@@ -7,13 +7,13 @@ import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.*;
 import szoeke.bence.kafkastreamprocessor.entity.Event;
 import szoeke.bence.kafkastreamprocessor.entity.innerentity.SipMessage;
 import szoeke.bence.kafkastreamprocessor.utility.EventDeserializer;
 import szoeke.bence.kafkastreamprocessor.utility.EventSerializer;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
@@ -21,31 +21,49 @@ import java.util.stream.Collectors;
 
 public class StreamProcessor {
 
+    private enum OPERATION_TYPE {
+        FILTER_AND_MAP,
+        FILTER_AND_WINDOWED_BY_AND_COUNT
+    }
+
     private static final List<String> IGNORABLE_FIELD_NAMES = List.of("From", "To", "Via");
     private static final String INPUT_TOPIC = "streams-input";
     private static final String OUTPUT_TOPIC = "streams-output";
+    private static final String OPERATION_TYPE_ENV_VAR = "OPERATION_TYPE";
+    private static final String BOOTSTRAP_SERVER_ENV_VAR = "BOOTSTRAP_SERVER";
     private final Properties properties;
     private final Serde<Event> eventSerde;
     private final Serde<Long> longSerde;
     private final StreamsBuilder builder;
+    private final OPERATION_TYPE operationType;
 
     public StreamProcessor(ObjectMapper objectMapper) {
-        eventSerde = Serdes.serdeFrom(new EventSerializer(objectMapper), new EventDeserializer(objectMapper));
+        this.eventSerde = Serdes.serdeFrom(new EventSerializer(objectMapper), new EventDeserializer(objectMapper));
         this.longSerde = Serdes.Long();
         this.builder = new StreamsBuilder();
         this.properties = new Properties();
         this.properties.putIfAbsent(StreamsConfig.APPLICATION_ID_CONFIG, "stream-processor");
-        this.properties.putIfAbsent(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        this.properties.putIfAbsent(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv(BOOTSTRAP_SERVER_ENV_VAR));
+        this.operationType = OPERATION_TYPE.valueOf(System.getenv(OPERATION_TYPE_ENV_VAR));
     }
 
     public void processEvents() {
-        defineMapOperations();
-//        defineWindowedByOperations();
-//        startOperations();
+        chooseOperation();
         startOperations();
     }
 
-    private void defineMapOperations() {
+    private void chooseOperation() {
+        switch (operationType) {
+            case FILTER_AND_MAP:
+                defineFilterAndMapOperations();
+                break;
+            case FILTER_AND_WINDOWED_BY_AND_COUNT:
+                defineFilterAndWindowedByAndCountOperations();
+                break;
+        }
+    }
+
+    private void defineFilterAndMapOperations() {
         builder
                 .stream(INPUT_TOPIC, Consumed.with(longSerde, eventSerde))
                 .filter(this::filterEventRecordHeaderResult)
@@ -54,18 +72,19 @@ public class StreamProcessor {
                 .to(OUTPUT_TOPIC, Produced.with(longSerde, eventSerde));
     }
 
-//    private void defineWindowedByOperations() {
-//        try (TimeWindowedSerializer<Long> serializer = new TimeWindowedSerializer<>(Serdes.Long().serializer());
-//             TimeWindowedDeserializer<Long> deserializer = new TimeWindowedDeserializer<>(Serdes.Long().deserializer())) {
-//            builder
-//                    .stream(INPUT_TOPIC, Consumed.with(longSerde, eventSerde))
-//                    .groupByKey()
-//                    .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofMillis(2500)))
-//                    .count()
-//                    .toStream()
-//                    .to(OUTPUT_TOPIC, Produced.with(Serdes.serdeFrom(serializer, deserializer), Serdes.Long()));
-//        }
-//    }
+    private void defineFilterAndWindowedByAndCountOperations() {
+        try (TimeWindowedSerializer<Long> serializer = new TimeWindowedSerializer<>(Serdes.Long().serializer());
+             TimeWindowedDeserializer<Long> deserializer = new TimeWindowedDeserializer<>(Serdes.Long().deserializer())) {
+            builder
+                    .stream(INPUT_TOPIC, Consumed.with(longSerde, eventSerde))
+                    .filter(this::filterEventRecordHeaderResult)
+                    .groupBy((key, event) -> event.eventRecordHeader.Cause.ErrorCode)
+                    .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofMillis(2500)))
+                    .count()
+                    .toStream()
+                    .to(OUTPUT_TOPIC, Produced.with(Serdes.serdeFrom(serializer, deserializer), Serdes.Long()));
+        }
+    }
 
     //filter by result
     private boolean filterEventRecordHeaderResult(Long key, Event event) {
@@ -94,13 +113,6 @@ public class StreamProcessor {
                 .collect(Collectors.toList());
         return sipMessage;
     }
-
-//    private void startOperations() {
-//        try (KafkaStreams streams = new KafkaStreams(builder.build(), properties)) {
-//            streams.start();
-//        } catch (Exception e) {
-//        }
-//    }
 
     private void startOperations() {
         try (KafkaStreams streams = new KafkaStreams(builder.build(), properties)) {

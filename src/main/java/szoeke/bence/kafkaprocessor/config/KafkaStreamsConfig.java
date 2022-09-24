0192e3cb8007320c2c61945303ba4021e13ddaf5
Szőke Bence <bence.szoke@idomsoft.hu>
2022-09-24 17:40:53 +0200

    add commit interval ms env var

--- src/main/java/szoeke/bence/kafkaprocessor/config/KafkaStreamsConfig.java
+++ src/main/java/szoeke/bence/kafkaprocessor/config/KafkaStreamsConfig.java
@@ -9,13 +9,15 @@ public class KafkaStreamsConfig {
     private static final String BOOTSTRAP_SERVER_ENV_VAR = "BOOTSTRAP_SERVER";
     private static final String NUM_STREAM_THREADS_ENV_VAR = "NUM_STREAM_THREADS";
     private static final String METRICS_RECORDING_LEVEL_CONFIG_ENV_VAR = "METRICS_RECORDING_LEVEL";
+    private static final String COMMIT_INTERVAL_MS_CONFIG_ENV_VAR = "COMMIT_INTERVAL_MS";
 
     public Properties generateConfig() {
         Properties properties = new Properties();
-        properties.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "stream-processor");
+        properties.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "kafkastreams-processor");
         properties.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv(BOOTSTRAP_SERVER_ENV_VAR));
         properties.setProperty(StreamsConfig.NUM_STREAM_THREADS_CONFIG, System.getenv(NUM_STREAM_THREADS_ENV_VAR));
         properties.setProperty(StreamsConfig.METRICS_RECORDING_LEVEL_CONFIG, System.getenv(METRICS_RECORDING_LEVEL_CONFIG_ENV_VAR));
+        properties.setProperty(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, System.getenv(COMMIT_INTERVAL_MS_CONFIG_ENV_VAR));
         return properties;
     }
 }

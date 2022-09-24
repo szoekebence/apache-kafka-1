f68b8faa2defa6a538500696d0d839d441ff3230
Szőke Bence <bence.szoke@idomsoft.hu>
2022-09-24 18:03:58 +0200

    fix config

--- src/main/java/szoeke/bence/kafkaprocessor/config/KafkaStreamsConfig.java
+++ src/main/java/szoeke/bence/kafkaprocessor/config/KafkaStreamsConfig.java
@@ -1,5 +1,6 @@
 package szoeke.bence.kafkaprocessor.config;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.streams.StreamsConfig;
 
 import java.util.Properties;
@@ -16,8 +17,18 @@ public class KafkaStreamsConfig {
         properties.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "kafkastreams-processor");
         properties.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv(BOOTSTRAP_SERVER_ENV_VAR));
         properties.setProperty(StreamsConfig.NUM_STREAM_THREADS_CONFIG, System.getenv(NUM_STREAM_THREADS_ENV_VAR));
-        properties.setProperty(StreamsConfig.METRICS_RECORDING_LEVEL_CONFIG, System.getenv(METRICS_RECORDING_LEVEL_CONFIG_ENV_VAR));
-        properties.setProperty(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, System.getenv(COMMIT_INTERVAL_MS_CONFIG_ENV_VAR));
+        setOptionalParameters(properties);
         return properties;
+    }
+
+    private static void setOptionalParameters(Properties properties) {
+        String metricsRecordingLevel = System.getenv(METRICS_RECORDING_LEVEL_CONFIG_ENV_VAR);
+        if (StringUtils.isNotBlank(metricsRecordingLevel)) {
+            properties.setProperty(StreamsConfig.METRICS_RECORDING_LEVEL_CONFIG, metricsRecordingLevel);
+        }
+        String commitIntervalMs = System.getenv(COMMIT_INTERVAL_MS_CONFIG_ENV_VAR);
+        if (StringUtils.isNotBlank(commitIntervalMs)) {
+            properties.setProperty(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, commitIntervalMs);
+        }
     }
 }
